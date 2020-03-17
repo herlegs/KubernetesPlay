c4cd07eaeed68c4c5ed4b5e9c62fb5829b015ac7
herlegs <xiaoyuguang24@gmail.com>
2020-03-17 22:04:14 +0800

    add k8s client

---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/herlegs/KubernetesPlay
+
+go 1.13
+
+require (
+	github.com/golang/protobuf v1.3.2
+	github.com/grpc-ecosystem/grpc-gateway v1.13.0
+	golang.org/x/net v0.0.0-20191004110552-13f9640d40b9
+	google.golang.org/genproto v0.0.0-20190927181202-20e1ac93f88c
+	google.golang.org/grpc v1.24.0
+	k8s.io/api v0.17.0
+	k8s.io/apimachinery v0.17.0
+	k8s.io/client-go v0.17.0
+)
--- simplehttpserver/main.go
+++ simplehttpserver/main.go
@@ -5,6 +5,12 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 
 	"github.com/herlegs/KubernetesPlay/serverutil"
 
@@ -14,18 +20,81 @@ import (
 	"google.golang.org/grpc"
 )
 
+var version = time.Now().In(time.FixedZone("GMT", 8*60*60)).Format("2006/01/02 15:04")
+
+var podCount = 1
+
 func main() {
-	fmt.Printf("starting hello server at [%v]...\n", serverutil.GetIPAddr())
+	initKubeClient()
+	fmt.Printf("starting hello server [%v] at [%v]...\n", version, serverutil.GetIPAddr())
 	http.HandleFunc("/", hello)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
 	}
 }
 
+//initKubeClient
+func initKubeClient() {
+	fmt.Printf("trying to init kube client...\n")
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		fmt.Printf("error getting kubernetes cluster config: %v\n", err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		fmt.Printf("error init kubernetes clientset: %v\n", err)
+	}
+	go func() {
+		//watchlist := cache.NewListWatchFromClient(clientset.CoreV1().RESTClient(), "pods", "test", fields.Everything())
+		//
+		//_, controller := cache.NewInformer(
+		//	watchlist,
+		//	&v1.Pod{},
+		//	time.Second*0,
+		//	cache.ResourceEventHandlerFuncs{
+		//		AddFunc: func(obj interface{}) {
+		//			fmt.Printf("add: %s \n", obj)
+		//		},
+		//		DeleteFunc: func(obj interface{}) {
+		//			fmt.Printf("delete: %s \n", obj)
+		//		},
+		//		UpdateFunc: func(oldObj, newObj interface{}) {
+		//			fmt.Printf("old: %s, new: %s \n", oldObj, newObj)
+		//		},
+		//	},
+		//)
+		//stop := make(chan struct{})
+		//go controller.Run(stop)
+
+		for {
+			ticker := time.NewTicker(time.Second * 5)
+			defer ticker.Stop()
+
+			for range ticker.C {
+				l, err := clientset.CoreV1().Pods("test").List(metav1.ListOptions{
+					LabelSelector: "app=helloapp",
+				})
+				if err != nil {
+					fmt.Printf("error calling api: %v\n", err)
+				}
+
+				fmt.Printf("try listing result...\n")
+				if l != nil {
+					for _, n := range l.Items {
+						fmt.Printf("items: %v\n", n.Name)
+					}
+				}
+			}
+		}
+
+	}()
+}
+
 func hello(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 	path = strings.TrimPrefix(path, "/")
-	message := fmt.Sprintf("Hello %v, I'm %v. %v", path, serverutil.GetIPAddr(), callCounterServerViaK8S(path))
+	message := fmt.Sprintf("version [%v] Hello %v, I'm %v.\n%v", version, path, serverutil.GetIPAddr(), callCounterServerViaK8S(path))
 	w.Write([]byte(message))
 }
 
